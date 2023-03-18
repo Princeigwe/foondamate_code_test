8a3914d5436ebd2819eee7d94aa33ff65aa1c66b
Prince Igwe <igwep297@gmail.com>
2023-03-18 13:56:59 +0100

    tried to make response more human readable with xmltodict package, but it just got worse. had to go back to the former response

--- queries/views.py
+++ queries/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from .serializers import MathQuerySerializer
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+import xml.etree.ElementTree as ET
+import xmltodict
+import json
 
 # Create your views here.
 
@@ -20,11 +23,3 @@ def solve_question(request):
         response = requests.get(url, params=parameters, headers=headers)
         return Response(response.content, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
-    # math_query_serializer = MathQuerySerializer(data=request.data)
-    # if math_query_serializer.is_valid():
-    #     url = 'http://api.wolframalpha.com/v2/query'
-    #     headers = {'Content-Type': 'application/xml'}
-    #     parameters = {'appid' : settings.WOLFRAM_API_KEY, 'podstate': 'Step-by-step%20solution', 'input': math_query_serializer}
-    #     response = requests.get(url, params=parameters, headers=headers)
-    #     return Response(response.content, status=status.HTTP_201_CREATED)
-    # return Response(math_query_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
