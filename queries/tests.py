562d0d75373a523699125bf970a8818edf577553
Prince Igwe <igwep297@gmail.com>
2023-03-18 17:45:41 +0100

    wrote test for math query endpoint

--- queries/tests.py
+++ queries/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
 
 # Create your tests here.
+
+class QueryTest(APITestCase):
+    def test_response_status_code(self):
+        params = {'math_query': "2x+7=9"}
+        url = reverse('math_query')
+        response = self.client.get(url, params=params)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- queries/urls.py
+++ queries/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import solve_question
 
 urlpatterns = [
-    path('', solve_question)
+    path('', solve_question, name='math_query')
 ]
